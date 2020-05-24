8c37303bb3cc5c47bc6297473e04312c5a115013
Atul Mehra <eng.amehra@gmail.com>
2020-05-24 05:37:27 +0530

    Updates

---
+++ precache-manifest.080d4c497e748e001af0ce8835053136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a08fe28ea7409c38ac57a279cf0eec",
+    "url": "/nirman-react-building-blocks/index.html"
+  },
+  {
+    "revision": "55f0539755c28cb8a79b",
+    "url": "/nirman-react-building-blocks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4a086f6b78bb2203fe86",
+    "url": "/nirman-react-building-blocks/static/js/2.c318b5f8.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/nirman-react-building-blocks/static/js/2.c318b5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f0539755c28cb8a79b",
+    "url": "/nirman-react-building-blocks/static/js/main.e78c2fbb.chunk.js"
+  },
+  {
+    "revision": "8b4e5ded2331ce8f2807",
+    "url": "/nirman-react-building-blocks/static/js/runtime-main.c4f6b375.js"
+  }
+]);
